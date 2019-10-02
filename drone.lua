dc51a97f59f9e582a7c244a01441b3e40f75e2fc
Diskordius <None>
2019-10-02 13:29:36 +0200

    Fixed integration for OS less drone.

--- drone.lua
+++ drone.lua
@@ -1,12 +1,11 @@
-local event = component.proxy(component.list("event"))
-local drone = component.proxy(component.list("drone"))
-local modem = component.proxy(component.list("modem"))
+local drone = component.proxy(component.list("drone")())
+local modem = component.proxy(component.list("modem")())
 
 modem.open(67)
 
 while true do
-  local _, _, from, _, _, message = event.pull("modem_message")
-  if message == "DISCOVER" then
+  local event, _, from, _, _, message = computer.pullSignal()
+  if (event == "modem_message" and message == "DISCOVER") then
     master = from
     break
   end
@@ -19,8 +18,8 @@ modem.open(23)
 modem.send(master, 22, "CONNECT")
 
 while true do
-  local _, _, from, port, _, message = event.pull("modem_message")
-  if (from == master and port == 22) then
+  local event, _, from, port, _, message = computer.pullSignal()
+  if (event == "modem_message" and from == master and port == 22) then
     local command = pcall(load(message))
     local result = command()
     modem.send(master, 22, result)
