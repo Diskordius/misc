e6c9bfec1c2f58f5532c5323df1b5f63f4aadde7
Diskordius <None>
2019-10-02 14:15:13 +0200

    Now can reconnect to running drone.

--- drone.lua
+++ drone.lua
@@ -11,7 +11,6 @@ while true do
   end
 end
 
-modem.close(67)
 modem.open(22)
 modem.open(23)
 
@@ -19,18 +18,22 @@ modem.send(master, 22, "CONNECT")
 
 while true do
   local event, _, from, port, _, message = computer.pullSignal()
-  if (event == "modem_message" and from == master and port == 22) then
-    local command = load(message)
-    local result = pcall(command)
-    modem.send(master, 22, result)
-  elseif (from == master and port == 23) then
-    if     message == 'x' then drone.move(  0,  1,  0)
-    elseif message == 'v' then drone.move(  0,  0, -1)
-    elseif message == 'l' then drone.move(  0, -1,  0)
-    elseif message == 'u' then drone.move( -1,  0,  0)
-    elseif message == 'i' then drone.move(  0,  0,  1)
-    elseif message == 'a' then drone.move(  1,  0,  0)
-    elseif message == 'e' then drone.swing(0)
-    elseif message == 'o' then drone.drop(0, 1) end
+  if (event == "modem_message" and from == master) then
+    if (port == 67 and message == "DISCOVER") then
+      modem.send(master, 22, "CONNECT")
+    elseif port == 22 then
+      local command = load(message)
+      local result = pcall(command)
+      modem.send(master, 22, result)
+    elseif port == 23 then
+      if     message == 'x' then drone.move(  0,  1,  0)
+      elseif message == 'v' then drone.move(  0,  0, -1)
+      elseif message == 'l' then drone.move(  0, -1,  0)
+      elseif message == 'u' then drone.move( -1,  0,  0)
+      elseif message == 'i' then drone.move(  0,  0,  1)
+      elseif message == 'a' then drone.move(  1,  0,  0)
+      elseif message == 'e' then drone.swing(0)
+      elseif message == 'o' then drone.drop(0, 1) end
+    end
   end
 end
